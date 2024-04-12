eafa12c6bf457e7457d89822c17596e8d22d4f7e
Junh Been <beenjh6088@gmail.com>
2024-04-12 13:04:13 +0900

    finishing layout

--- js/common.js
+++ js/common.js
@@ -4,16 +4,16 @@ window.onload = function() {
   let sts_headerLogoBtn;
 
   headerLogoBtn.addEventListener("click", function() {
-    sts_headerLogoBtn = headerLogoBtn.className;
-    if(sts_headerLogoBtn == "up") {
-      headerLogoBtn.classList.remove("up");
-      headerLogoBtn.classList.add("down");
-      logoBtnMenu.style.display = "block";
-    }else if(sts_headerLogoBtn == "down") {
-      headerLogoBtn.classList.remove("down");
-      headerLogoBtn.classList.add("up");
-      logoBtnMenu.style.display = "none";
-    }
+    // sts_headerLogoBtn = headerLogoBtn.className;
+    // if(sts_headerLogoBtn == "up") {
+    //   headerLogoBtn.classList.remove("up");
+    //   headerLogoBtn.classList.add("down");
+    //   logoBtnMenu.style.display = "block";
+    // }else if(sts_headerLogoBtn == "down") {
+    //   headerLogoBtn.classList.remove("down");
+    //   headerLogoBtn.classList.add("up");
+    //   logoBtnMenu.style.display = "none";
+    // }
   });
   
 }
