4b691f2a9df4f9247b4fc1777c49255b716055dd
marbeen <84909524+beenjh6088@users.noreply.github.com>
2024-04-26 01:07:06 +0900

    error on backward and forward clicking consecutively

--- js/common.js
+++ js/common.js
@@ -1,19 +1,71 @@
-window.onload = function() {
-  // const headerLogoBtn = document.getElementById("headerLogoBtn");
-  // const logoBtnMenu = document.getElementById("logoBtnMenu");
-  // let sts_headerLogoBtn;
+$(function() {
+  const $rcm_imagesets = $("#recommen .slider .imagesets");
+  const $rcm_locations = $("#recommen .locations ul");
+  const $gnb_mainMenu = $("#gnb .mainMenu");
+  let num_sliderIndex = 0;
+  let slideRotation;
+  let bln_ongoingRotaion = false;
 
-  // headerLogoBtn.addEventListener("click", function() {
-    // sts_headerLogoBtn = headerLogoBtn.className;
-    // if(sts_headerLogoBtn == "up") {
-    //   headerLogoBtn.classList.remove("up");
-    //   headerLogoBtn.classList.add("down");
-    //   logoBtnMenu.style.display = "block";
-    // }else if(sts_headerLogoBtn == "down") {
-    //   headerLogoBtn.classList.remove("down");
-    //   headerLogoBtn.classList.add("up");
-    //   logoBtnMenu.style.display = "none";
-    // }
-  // });
+
+  // header navi
+  $("li", $gnb_mainMenu).mouseover(function() {
+    $(".submenu").stop().slideDown(300);
+    $(".top").css({
+      'height':'240px'
+    })
+  }).mouseleave(function() {
+    $(".submenu").stop().slideUp(300);
+    $(".top").css({
+      'height':'90px'
+    })
+  })
+
   
-}
+  // recommen slider
+  slideRotation= setInterval(() => {
+    if(bln_ongoingRotaion == false) {
+      $rcm_imagesets.animate({
+        'marginLeft':'-675px'
+      }, function() {
+        $(".image:first", this).appendTo(this);
+        $(this).css({ 'marginLeft':'0' });
+  
+        // tracking location
+        num_sliderIndex = (++num_sliderIndex) % $rcm_locations.children().length;
+        $rcm_locations.children("li:eq("+num_sliderIndex+")").addClass("active")
+        .siblings().removeClass("active");
+      })
+    }
+  }, 3000);
+  setInterval(() => {
+    if(bln_ongoingRotaion == true) bln_ongoingRotaion = false;
+  }, 2000);
+
+  $(".arrows .next").click(function() {
+    // clearInterval(slideRotation);
+    bln_ongoingRotaion = true;
+    $rcm_imagesets.stop().animate({
+      'marginLeft':'-675px'
+    }, function() {
+      $(".image:first", this).appendTo(this);
+      $(this).css({ 'marginLeft':'0' });
+
+      // tracking location
+      num_sliderIndex = (++num_sliderIndex) % $rcm_locations.children().length;
+      $rcm_locations.children("li:eq("+num_sliderIndex+")").addClass("active")
+      .siblings().removeClass("active");
+    })
+  });
+  $(".arrows .prev").click(function() {
+    // clearInterval(slideRotation);
+    bln_ongoingRotaion = true;
+    num_sliderIndex == 0 ? num_sliderIndex = $rcm_locations.children().length-1 : num_sliderIndex--;
+    // console.log(num_sliderIndex)
+    $rcm_imagesets.animate({
+      'marginLeft':(-675)*num_sliderIndex+"px"
+    })
+    $rcm_locations.children("li:eq("+num_sliderIndex+")").addClass("active")
+      .siblings().removeClass("active");
+  })
+
+});
