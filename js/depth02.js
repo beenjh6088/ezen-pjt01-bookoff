93b8f2930de9f528a15f9b6fb8b9876e038b85d1
marbeen <84909524+beenjh6088@users.noreply.github.com>
2024-05-15 11:38:21 +0900

    adding an active function on category in filter.html

--- js/depth02.js
+++ js/depth02.js
@@ -84,6 +84,7 @@ function fetchData() {
           let lDepth03 = document.createElement("li");
           lDepth03.classList.add("depth");
           lDepth03.classList.add("depth03");
+          arr_depth03[i] == depth03 ? lDepth03.classList.add("active") : null;
           lDepth03.innerHTML = `<a href='#'>${arr_depth03[i]}<span>(${new Intl.NumberFormat().format(obj_depth03[i][''+arr_depth03[i]])})</span></a>`
           li.parentElement.appendChild(lDepth03);
         }
